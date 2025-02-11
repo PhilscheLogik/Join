f4a891531d9ff7cf50899d2476023334a898b3a7
Philipp Franke <Franke.Phil89@googlemail.com>
2025-02-11 16:34:03 +0100

    added header/footer and 4 sections in content via ids

--- src/app/main/content/content.component.ts
+++ src/app/main/content/content.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
+import { AddTaskComponent } from './add-task/add-task.component';
+import { BoardComponent } from './board/board.component';
+import { SummaryComponent } from './summary/summary.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { FooterComponent } from '../../shared/footer/footer.component';
+import { HeaderComponent } from "../../shared/header/header.component";
 
 @Component({
   selector: 'app-content',
   standalone: true,
-  imports: [],
+  imports: [SummaryComponent, AddTaskComponent, BoardComponent, ContactsComponent, FooterComponent, HeaderComponent],
   templateUrl: './content.component.html',
   styleUrl: './content.component.scss'
 })
