72687a9216637ccf45e08c0bbb473e94ddd89016
Philipp Franke <Franke.Phil89@googlemail.com>
2025-02-11 20:17:37 +0100

    Navbar in footer desktop designed

--- src/app/main/content/content.component.ts
+++ src/app/main/content/content.component.ts
@@ -4,15 +4,20 @@ import { BoardComponent } from './board/board.component';
 import { SummaryComponent } from './summary/summary.component';
 import { ContactsComponent } from './contacts/contacts.component';
 import { FooterComponent } from '../../shared/footer/footer.component';
-import { HeaderComponent } from "../../shared/header/header.component";
+import { HeaderComponent } from '../../shared/header/header.component';
 
 @Component({
   selector: 'app-content',
   standalone: true,
-  imports: [SummaryComponent, AddTaskComponent, BoardComponent, ContactsComponent, FooterComponent, HeaderComponent],
+  imports: [
+    SummaryComponent,
+    AddTaskComponent,
+    BoardComponent,
+    ContactsComponent,
+    FooterComponent,
+    HeaderComponent,
+  ],
   templateUrl: './content.component.html',
-  styleUrl: './content.component.scss'
+  styleUrl: './content.component.scss',
 })
-export class ContentComponent {
-
-}
+export class ContentComponent {}
--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,12 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
+  selectedItem: number = 0;
+
+  selectItem(num: number) {
+    this.selectedItem = num;
+  }
 
 }
