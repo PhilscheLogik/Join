30159afa5d0a6d0bb9257a78a95f6ebf7b628d6c
Philipp Franke <Franke.Phil89@googlemail.com>
2025-02-11 08:48:26 +0100

    first structure

---
+++ src/app/main/main.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { LoginComponent } from './login/login.component';
+import { ContentComponent } from './content/content.component';
+
+@Component({
+  selector: 'app-main',
+  standalone: true,
+  imports: [LoginComponent, ContentComponent],
+  templateUrl: './main.component.html',
+  styleUrl: './main.component.scss'
+})
+export class MainComponent {
+
+}
