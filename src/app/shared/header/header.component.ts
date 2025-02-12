9709a220c9e4f4a16913cea4883c43d76017f444
Dominik Marnet <oxgameboyxo@googlemail.com>
2025-02-12 14:26:10 +0100

    added: toggleOverlay() + ngClass for header overlay

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+
+  overlayState = true;
+  
+  toggleOverlay() {
+    this.overlayState = !this.overlayState;
+  }
 
 }
