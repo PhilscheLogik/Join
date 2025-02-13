dced6a8fc75fbf3f55bfd4115c8746b4b2b270d6
Normann Blum <normannblum@gmail.com>
2025-02-13 15:49:56 +0100

    add contacts function test

--- src/app/main/login/login.component.ts
+++ src/app/main/login/login.component.ts
@@ -1,12 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  // isDialogOpen: boolean = false;
 
+  // openDialog(): void {
+  //   this.isDialogOpen = true;
+  // }
+
+  // closeDialog(): void {
+  //   this.isDialogOpen = false;
+  // }
 }
