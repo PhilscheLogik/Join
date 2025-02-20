7c968e4c43bd17442d5ccc6207a76861cb3f5ac0
Dominik Marnet <oxgameboyxo@googlemail.com>
2025-02-20 12:03:26 +0100

    added: drag & drop board compnent

--- src/app/main/content/board/board.component.ts
+++ src/app/main/content/board/board.component.ts
@@ -1,14 +1,40 @@
 import { Component, inject } from '@angular/core';
 import { SingleTaskComponent } from './single-task/single-task.component';
+import {
+  CdkDragDrop,
+  moveItemInArray,
+  transferArrayItem,
+  CdkDrag,
+  CdkDropList,
+} from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-board',
   standalone: true,
-  imports: [SingleTaskComponent],
+  imports: [SingleTaskComponent, CdkDropList, CdkDrag],
   templateUrl: './board.component.html',
   styleUrl: './board.component.scss'
 })
 export class BoardComponent {
   
+  todo = ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep'];
 
+  progress = ['Get to work', 'Pick up groceries']
+
+  feedback = ['Go home', 'Fall asleep']
+
+  done = ['Get up', 'Brush teeth', 'Take a shower', 'Check e-mail', 'Walk dog'];
+
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    }
+  }
 }
