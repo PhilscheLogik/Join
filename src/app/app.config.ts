ca1d4be32abf263aa187210bc34425a3b905119e
Martin Bock <martinbock@arcor.de>
2025-02-20 10:53:50 +0100

    package angular material installed

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,7 +4,8 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"joindb-4dd40","appId":"1:550188192810:web:2df3a711daf5ca6bb95f53","storageBucket":"joindb-4dd40.firebasestorage.app","apiKey":"AIzaSyBj1vvXI8VUMvYCd_0PCtdVIZh70n4iYEA","authDomain":"joindb-4dd40.firebaseapp.com","messagingSenderId":"550188192810"}))), importProvidersFrom(provideFirestore(() => getFirestore()))]
+  providers: [provideRouter(routes), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"joindb-4dd40","appId":"1:550188192810:web:2df3a711daf5ca6bb95f53","storageBucket":"joindb-4dd40.firebasestorage.app","apiKey":"AIzaSyBj1vvXI8VUMvYCd_0PCtdVIZh70n4iYEA","authDomain":"joindb-4dd40.firebaseapp.com","messagingSenderId":"550188192810"}))), importProvidersFrom(provideFirestore(() => getFirestore())), provideAnimationsAsync()]
 };
