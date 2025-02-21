f07f82f252d474ee2c75e32f998484c2fce20b65
Philipp Franke <Franke.Phil89@googlemail.com>
2025-02-21 15:46:12 +0100

    Merge Conflict and added function for Substasks

--- src/app/main/content/add-task/add-task.component.ts
+++ src/app/main/content/add-task/add-task.component.ts
@@ -3,11 +3,12 @@ import { TaskServiceService } from '../../services/task-service.service';
 import { ContactsService } from '../../services/contacts.service';
 import { Firestore } from '@angular/fire/firestore';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-add-task',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,FormsModule],
   templateUrl: './add-task.component.html',
   styleUrl: './add-task.component.scss',
 })
@@ -35,4 +36,48 @@ export class AddTaskComponent {
       (c) => c.email === contact.email
     );
   }
+
+
+  /*new */
+  newSubtask: string = '';
+  subtasks: { text: string; isEditing: boolean }[] = [{ text:'Taste Esc suchen',isEditing: false }, { text:'Schuhe zubinden',isEditing: false },];
+  isEditing: boolean = false;
+
+  startEditing() {
+    this.isEditing = true;
+  }
+
+  // stopEditing() {
+  //   this.isEditing = false;
+  // }
+
+  cancelEditing() {
+    this.isEditing = false;
+    this.newSubtask = '';
+  }
+
+  addSubtask() {
+    if (this.newSubtask.trim()) {
+      this.subtasks.push({ text: this.newSubtask, isEditing: false });
+    }
+    this.cancelEditing();
+  }
+
+  deleteSubtask(index: number) {
+    this.subtasks.splice(index, 1);
+  }
+
+  editSubtask(index: number) {
+    this.subtasks[index].isEditing = true;
+  }
+
+  saveEdit(index: number) {
+    if (!this.subtasks[index].text.trim()) {
+      this.subtasks[index].text = this.subtasks[index].text;
+    }
+    this.subtasks[index].isEditing = false;
+  }
 }
+
+
+
