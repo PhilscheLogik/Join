344ec9587b505082a95f649b8219cb81b1b238cf
Normann Blum <normannblum@gmail.com>
2025-02-21 16:07:15 +0100

    single-task small card added and service connection

--- src/app/main/content/board/single-task/single-task.component.ts
+++ src/app/main/content/board/single-task/single-task.component.ts
@@ -1,18 +1,34 @@
 import { Component, inject } from '@angular/core';
 import { TaskServiceService } from '../../../services/task-service.service';
+import { Task } from '../../../../interfaces/task';
 
 @Component({
   selector: 'app-single-task',
   standalone: true,
   imports: [],
   templateUrl: './single-task.component.html',
-  styleUrl: './single-task.component.scss'
+  styleUrl: './single-task.component.scss',
 })
-export class SingleTaskComponent {
-  taskService = inject(TaskServiceService);
-  
-    // test() {
-    //   console.log(this.taskService.taskList);
-    // }
 
+export class SingleTaskComponent {
+  private taskService = inject(TaskServiceService);
+  tasks: Task[] = [];
+  selectedTask: Task | null = null;
+  isOverlayOpen: boolean = false;
+
+  constructor() {
+    this.tasks = this.taskService.todoList;
+  }
+
+  openOverlay(task: Task) {
+    this.selectedTask = task;
+    this.isOverlayOpen = true;
+  }
+
+  closeOverlay() {
+    this.isOverlayOpen = false;
+  }
 }
+// test() {
+//   console.log(this.taskService.taskList);
+// }
