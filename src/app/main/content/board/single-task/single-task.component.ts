642bbe2cbf188384e2f5760b6cb87dccf7c3ecd8
Normann Blum <normannblum@gmail.com>
2025-02-24 18:04:30 +0100

    initial icon implementing small cards

--- src/app/main/content/board/single-task/single-task.component.ts
+++ src/app/main/content/board/single-task/single-task.component.ts
@@ -8,21 +8,24 @@ import {
   CdkDrag,
   CdkDropList,
 } from '@angular/cdk/drag-drop';
-
+import { CommonModule } from '@angular/common';
+import { ContactsService } from '../../../services/contacts.service';
 
 @Component({
   selector: 'app-single-task',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './single-task.component.html',
   styleUrl: './single-task.component.scss',
 })
-
 export class SingleTaskComponent {
   private taskService = inject(TaskServiceService);
+  contactService = inject(ContactsService);
+
   tasks: Task[] = [];
   selectedTask: Task | null = null;
   isOverlayOpen: boolean = false;
+  selectedContacts: any[] = []; // Array für ausgewählte Kontakte
 
   @Input() task!: Task;
 
@@ -40,81 +43,95 @@ export class SingleTaskComponent {
   }
 
   /**
-     * Handles the drag-and-drop functionality for tasks.
-     * Moves items within the same list or transfers them between different lists.
-     *
-     * @param {CdkDragDrop<Task[]>} event - The drag-and-drop event containing task data.
-     */
-    async drop(event: CdkDragDrop<Task[]>) {
-      if (event.previousContainer === event.container) {
-        moveItemInArray(
-          event.container.data,
-          event.previousIndex,
-          event.currentIndex
-        );
-        // console.log(event.container.data, event.previousIndex, event.currentIndex);
-      } else {
-        const task: Task = event.previousContainer.data[event.previousIndex];
-        const previousCategory = this.getCategoryFromContainer(
-          event.previousContainer
-        );
-        const newCategory = this.getCategoryFromContainer(event.container);
-  
-        console.info('----- selected Task Board TS -------');
-        console.log(task);
-        console.log('Wo soll es hin: ', newCategory);
-  
-        /**
-         * Adds the task to the new category in the database.
-         * This ensures that the task is properly stored under its new status.
-         *
-         * @param {string} newCategory - The category to which the task is being moved.
-         * @param {Task} task - The task being updated.
-         */
-        await this.taskService.addTask(newCategory, task);
-  
-        /**
-         * Removes the task from its previous category in the database.
-         * This prevents duplicate tasks from appearing across different categories.
-         *
-         * @param {string} previousCategory - The category from which the task is being removed.
-         * @param {string} task.id - The unique identifier of the task.
-         */
-        if (task.id) {
-          console.log('Woher kam es: ', previousCategory);
-          await this.taskService.deleteTask(previousCategory, task.id);
-        }
-  
-        // Aktualisiere die lokalen Arrays
-        // transferArrayItem(
-        //   event.previousContainer.data,
-        //   event.container.data,
-        //   event.previousIndex,
-        //   event.currentIndex
-        // );
-  
-        // console.log(
-        //   event.previousContainer.data,
-        //   event.container.data,
-        //   event.previousIndex,
-        //   event.currentIndex,
-        // );
+   * Handles the drag-and-drop functionality for tasks.
+   * Moves items within the same list or transfers them between different lists.
+   *
+   * @param {CdkDragDrop<Task[]>} event - The drag-and-drop event containing task data.
+   */
+  async drop(event: CdkDragDrop<Task[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+      // console.log(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      const task: Task = event.previousContainer.data[event.previousIndex];
+      const previousCategory = this.getCategoryFromContainer(
+        event.previousContainer
+      );
+      const newCategory = this.getCategoryFromContainer(event.container);
+
+      console.info('----- selected Task Board TS -------');
+      console.log(task);
+      console.log('Wo soll es hin: ', newCategory);
+
+      /**
+       * Adds the task to the new category in the database.
+       * This ensures that the task is properly stored under its new status.
+       *
+       * @param {string} newCategory - The category to which the task is being moved.
+       * @param {Task} task - The task being updated.
+       */
+      await this.taskService.addTask(newCategory, task);
+
+      /**
+       * Removes the task from its previous category in the database.
+       * This prevents duplicate tasks from appearing across different categories.
+       *
+       * @param {string} previousCategory - The category from which the task is being removed.
+       * @param {string} task.id - The unique identifier of the task.
+       */
+      if (task.id) {
+        console.log('Woher kam es: ', previousCategory);
+        await this.taskService.deleteTask(previousCategory, task.id);
       }
+
+      // Aktualisiere die lokalen Arrays
+      // transferArrayItem(
+      //   event.previousContainer.data,
+      //   event.container.data,
+      //   event.previousIndex,
+      //   event.currentIndex
+      // );
+
+      // console.log(
+      //   event.previousContainer.data,
+      //   event.container.data,
+      //   event.previousIndex,
+      //   event.currentIndex,
+      // );
     }
-  
-    /**
-     * Determines the category of a task based on the provided drop list container.
-     *
-     * @param {CdkDropList} container - The drag-and-drop container.
-     * @returns {string} - The category of the task.
-     */
-    getCategoryFromContainer(container: CdkDropList): string {
-      if (container.data === this.taskService.todoList) return 'todo';
-      if (container.data === this.taskService.progressList) return 'inprogress';
-      if (container.data === this.taskService.feedbackList) return 'feedback';
-      if (container.data === this.taskService.doneList) return 'done';
-      return '';
-    }
+  }
+
+  /**
+   * Determines the category of a task based on the provided drop list container.
+   *
+   * @param {CdkDropList} container - The drag-and-drop container.
+   * @returns {string} - The category of the task.
+   */
+  getCategoryFromContainer(container: CdkDropList): string {
+    if (container.data === this.taskService.todoList) return 'todo';
+    if (container.data === this.taskService.progressList) return 'inprogress';
+    if (container.data === this.taskService.feedbackList) return 'feedback';
+    if (container.data === this.taskService.doneList) return 'done';
+    return '';
+  }
+
+  /**
+   * Finds the index of a contact in the full contact list based on the contact's email.
+   *
+   * @param {Object} contact - The contact whose index is to be found.
+   * @param {string} contact.email - The email address of the contact to search for.
+   *
+   * @returns {number} - Returns the index of the contact in the contact list, or `-1` if the contact is not found.
+   */
+  getIndexInFullList(contact: any): number {
+    return this.contactService.contactList.findIndex(
+      (c) => c.email === contact.email
+    );
+  }
 }
 // test() {
 //   console.log(this.taskService.taskList);
