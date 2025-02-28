ae6923d5d8c9b0eb542a4cde289dbfe8ee1c8a57
Philipp Franke <Franke.Phil89@googlemail.com>
2025-02-28 11:37:48 +0100

    Added function for Subtask using Enter

--- src/app/main/content/summary/summary.component.ts
+++ src/app/main/content/summary/summary.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Firestore } from '@angular/fire/firestore';
+import { TaskServiceService } from '../../services/task-service.service';
 
 @Component({
   selector: 'app-summary',
   standalone: true,
   imports: [],
   templateUrl: './summary.component.html',
-  styleUrl: './summary.component.scss'
+  styleUrl: './summary.component.scss',
 })
 export class SummaryComponent {
+  firestore: Firestore = inject(Firestore);
+  taskService = inject(TaskServiceService);
 
+  get() {
+    console.log(this.taskService.todoList);
+    console.log(this.taskService.progressList);
+    console.log(this.taskService.feedbackList);
+    console.log(this.taskService.doneList);
+  }
+
+  get2() {
+    
+    console.log(this.taskService.todoList);
+    console.log(this.taskService.progressList);
+    console.log(this.taskService.feedbackList);
+    console.log(this.taskService.doneList);
+  }
 }
