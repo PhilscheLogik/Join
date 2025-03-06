792d202d7d7d92ebebac322f2d4803ccd55ee291
Martin Bock <martinbock@arcor.de>
2025-03-06 11:25:38 +0100

    login style fixes

--- src/app/main/login/login.component.ts
+++ src/app/main/login/login.component.ts
@@ -1,13 +1,27 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { NavigationService } from '../../shared/navi/navigation.service';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
 export class LoginComponent {
+  navigationService = inject(NavigationService);
 
+  /**
+   * Selects an item by its index.
+   *
+   * This method sets the selected item in the navigation service based on the provided index. It updates the currently
+   * selected item to the one corresponding to the given index.
+   *
+   * @param {number} index - The index of the item to be selected.
+   * @returns {void} This method does not return anything.
+   */
+  selectItem(index: number) {
+    this.navigationService.setSelectedItem(index);
+  }
 }
