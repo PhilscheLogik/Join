9322c6112cfbb3f95643d54781a4ad7cf6577fa7
Martin Bock <martinbock@arcor.de>
2025-03-06 15:47:58 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -7,19 +7,21 @@ import { AppComponent } from './app/app.component';
 // https://firebase.google.com/docs/web/setup#available-libraries
 import { initializeApp } from 'firebase/app';
 
+// Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: 'AIzaSyBj1vvXI8VUMvYCd_0PCtdVIZh70n4iYEA',
+//   authDomain: 'joindb-4dd40.firebaseapp.com',
+//   projectId: 'joindb-4dd40',
+//   storageBucket: 'joindb-4dd40.firebasestorage.app',
+//   messagingSenderId: '550188192810',
+//   appId: '1:550188192810:web:2df3a711daf5ca6bb95f53',
+// };
+
+// Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+
+// export { app };
+
 bootstrapApplication(AppComponent, appConfig).catch((err) =>
   console.error(err)
 );
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: 'AIzaSyBj1vvXI8VUMvYCd_0PCtdVIZh70n4iYEA',
-  authDomain: 'joindb-4dd40.firebaseapp.com',
-  projectId: 'joindb-4dd40',
-  storageBucket: 'joindb-4dd40.firebasestorage.app',
-  messagingSenderId: '550188192810',
-  appId: '1:550188192810:web:2df3a711daf5ca6bb95f53',
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
