9de2f8afa3210213a1c2383c4b2980dc3884d5fe
Martin Bock <martinbock@arcor.de>
2025-03-06 15:47:26 +0100

    Update auth.service.ts

--- src/app/main/services/auth.service.ts
+++ src/app/main/services/auth.service.ts
@@ -6,16 +6,23 @@ import {
   signInWithEmailAndPassword,
 } from 'firebase/auth';
 
+ const firebaseConfig = {
+   apiKey: 'AIzaSyBj1vvXI8VUMvYCd_0PCtdVIZh70n4iYEA',
+   authDomain: 'joindb-4dd40.firebaseapp.com',
+   projectId: 'joindb-4dd40',
+   storageBucket: 'joindb-4dd40.firebasestorage.app',
+   messagingSenderId: '550188192810',
+   appId: '1:550188192810:web:2df3a711daf5ca6bb95f53',
+ };
+ // Initialize Firebase
+ const app = initializeApp(firebaseConfig);
+ 
+
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  firebaseConfig = {
-    // ...
-  };
-  // Initialize Firebase
-  // app = initializeApp(this.firebaseConfig);
-  auth = getAuth();
+  auth = getAuth(app);
   email = 'conan@tester.de';
   password = 'D1#tester';
 
