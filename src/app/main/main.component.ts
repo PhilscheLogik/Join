080f1ec3dcf945f18c3900b7582066cc510e4154
Dominik Marnet <oxgameboyxo@googlemail.com>
2025-03-06 11:35:05 +0100

    implement app signup to main html

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { LoginComponent } from './login/login.component';
 import { ContentComponent } from './content/content.component';
+import { SignupComponent } from './login/signup/signup.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [LoginComponent, ContentComponent],
+  imports: [LoginComponent, ContentComponent, SignupComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss',
 })
