6f3080c18c582f41919c6cf7d7be3405d8f75b73
Dominik Marnet <oxgameboyxo@googlemail.com>
2025-03-06 20:31:49 +0100

    added: signUp component html + scss

--- src/app/main/login/signup/signup.component.ts
+++ src/app/main/login/signup/signup.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component ,inject } from '@angular/core';
+import { NavigationService } from '../../../shared/navi/navigation.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.scss'
 })
 export class SignupComponent {
+  navigationService = inject(NavigationService);
 
+  /**
+   * Selects an item by its index.
+   *
+   * This method sets the selected item in the navigation service based on the provided index. It updates the currently
+   * selected item to the one corresponding to the given index.
+   *
+   * @param {number} index - The index of the item to be selected.
+   * @returns {void} This method does not return anything.
+   */
+  selectItem(index: number) {
+    this.navigationService.setSelectedItem(index);
+  }
 }
