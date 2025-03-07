40a35609f04b63eb02e9c4a5182b6db384298f8c
Philipp Franke <Franke.Phil89@googlemail.com>
2025-03-07 07:31:39 +0100

    added first elements, functions and design for the summary

--- src/app/main/content/summary/summary.component.ts
+++ src/app/main/content/summary/summary.component.ts
@@ -2,6 +2,8 @@ import { Component, inject } from '@angular/core';
 import { Firestore } from '@angular/fire/firestore';
 import { TaskServiceService } from '../../services/task-service.service';
 import { Task } from '../../../interfaces/task';
+import { NavigationService } from '../../../shared/navi/navigation.service';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-summary',
@@ -13,4 +15,64 @@ import { Task } from '../../../interfaces/task';
 export class SummaryComponent {
   firestore: Firestore = inject(Firestore);
   taskService = inject(TaskServiceService);
+  navigationService = inject(NavigationService);
+  authService = inject(AuthService);
+
+  dateDeadline = '2100-12-31';
+
+  getGreeting(username = '') {
+    const now = new Date();
+    const hour = now.getHours();
+    let greeting = '';
+
+    if (hour >= 6 && hour < 12) {
+      greeting = 'Good Morning';
+    } else if (hour >= 12 && hour < 18) {
+      greeting = 'Good day';
+    } else if (hour >= 18 && hour < 22) {
+      greeting = 'Good evening';
+    } else {
+      greeting = 'Welcome';
+    }
+    return greeting;
+  }
+
+  countUrgentTasks(): number {
+    let sum = 0;
+    let toDoUrgent = this.taskService.todoList?.filter(
+      (item) => item.prio === 'Urgent'
+    );
+    let progressUrgent = this.taskService.progressList?.filter(
+      (item) => item.prio === 'Urgent'
+    );
+    let feedbackUrgent = this.taskService.feedbackList?.filter(
+      (item) => item.prio === 'Urgent'
+    );
+
+    this.dateDeadline = '2100-12-31';
+    this.findNextDeadline(toDoUrgent);
+    this.findNextDeadline(progressUrgent);
+    this.findNextDeadline(feedbackUrgent);
+
+    sum += toDoUrgent.length ?? 0;
+    sum += progressUrgent.length ?? 0;
+    sum += feedbackUrgent.length ?? 0;
+    return sum;
+  }
+
+  findNextDeadline(array: any) {
+    if (!array || array.length === 0) {
+      return;
+    }
+
+    array.forEach((item: { date: string }) => {
+      if (this.getDeadlineDate() > new Date(item.date)) {
+        this.dateDeadline = String(item.date);
+      }
+    });
+  }
+
+  getDeadlineDate() {
+    return new Date(this.dateDeadline);
+  }
 }
--- src/app/main/services/auth.service.ts
+++ src/app/main/services/auth.service.ts
@@ -6,17 +6,16 @@ import {
   signInWithEmailAndPassword,
 } from 'firebase/auth';
 
- const firebaseConfig = {
-   apiKey: 'AIzaSyBj1vvXI8VUMvYCd_0PCtdVIZh70n4iYEA',
-   authDomain: 'joindb-4dd40.firebaseapp.com',
-   projectId: 'joindb-4dd40',
-   storageBucket: 'joindb-4dd40.firebasestorage.app',
-   messagingSenderId: '550188192810',
-   appId: '1:550188192810:web:2df3a711daf5ca6bb95f53',
- };
- // Initialize Firebase
- const app = initializeApp(firebaseConfig);
- 
+const firebaseConfig = {
+  apiKey: 'AIzaSyBj1vvXI8VUMvYCd_0PCtdVIZh70n4iYEA',
+  authDomain: 'joindb-4dd40.firebaseapp.com',
+  projectId: 'joindb-4dd40',
+  storageBucket: 'joindb-4dd40.firebasestorage.app',
+  messagingSenderId: '550188192810',
+  appId: '1:550188192810:web:2df3a711daf5ca6bb95f53',
+};
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 @Injectable({
   providedIn: 'root',
@@ -25,6 +24,7 @@ export class AuthService {
   auth = getAuth(app);
   email = 'conan@tester.de';
   password = 'D1#tester';
+  name = 'Ingo Düsenjäger';
 
   constructor() {}
 
